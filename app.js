6fa0e5ec4c094d721fb65751cda327ce3ac4db00
tanviriqbal <tanviriqbal4u@live.com>
2018-04-28 06:15:17 +0600

    Game Logic: New Game

--- app.js
+++ app.js
@@ -10,16 +10,7 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // Random Number
@@ -58,6 +49,27 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     }
     
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player-1';
+    document.getElementById('name-1').textContent = 'Player-2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 function generateRandomNumber() {
     return Math.floor(Math.random() * ((6-1)+1)) + 1;
